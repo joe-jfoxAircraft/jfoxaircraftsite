3428db227f7a88abab13df3c2c6e9050d0a6c713
Apiwat Joe Tee'r <53082433+joe-jfoxAircraft@users.noreply.github.com>
2019-07-23 21:23:47 +1000

    Patch (Responsive design)
    js

--- js/index.js
+++ js/index.js
@@ -11,6 +11,16 @@ function myFunction() {
   }
 }
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function popResponMenu() {
+  var x = document.getElementById("mainNav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 
 function backButton() {
   window.history.back();
